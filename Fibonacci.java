68f75ec4da1eb46d2d0a564c5d0b07b21d6fb8bf
Rutul Trivedi <114494847+RutulTrivedi@users.noreply.github.com>
2023-07-13 18:53:49 +0530

    Java program to find factorial of a number using recursion.

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,23 +1,41 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Fibonacci
 {
 	public static void main(String[] args)
 	{
-		int a=0,b=1,c,i,n;
 		Scanner sc=new Scanner(System.in);
+		seriesFib ob=new seriesFib();
+		int a=0,b=1;
 
-		System.out.print("Enter the number of numbers : ");
-		n=sc.nextInt();
-		System.out.print(" "+a);
-		System.out.print(" "+b);
+		System.out.print("Enter the number till which the number should be printed : ");
+		int n=sc.nextInt();
 
-		for(i=1;i<=n-2;i++)
+		System.out.print(a+" ");
+		System.out.print(b+" ");
+
+		ob.printSeries(a,b,n);
+	}
+}
+
+class seriesFib
+{
+	static int i=2;
+	public int printSeries(int a,int b,int n)
+	{
+		int c=0;
+
+		if(i==n)
+		{
+			return 0;
+		}
+		else
 		{
 			c=a+b;
-			a=b;
-			b=c;
-			System.out.print(" "+c);	
+			i++;
+			System.out.print(c+" ");
+			return printSeries(b,c,n);
 		}
+
 	}
 }
